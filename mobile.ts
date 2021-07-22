efd71bc4a18fca301b431b402a0249a5db7b4fad
fpicong71 <fpicong@yahoo.es>
2021-07-22 12:27:10 +0200

    feat(mobile, testMobile):Se han hecho los atributos privados y generado getters & setters. Método print, imprime caracteristicas

--- mobile.ts
+++ mobile.ts
@@ -1,37 +1,133 @@
 export class Mobile
 {
     //ATRIBUTOS
-    name:string;
-    model:string;
-    trademark:string;
-    sdSize:number;
-    color:string;
-    is5G:boolean;
-    cameraNumber:number;
-    price:number;
+    private name:string;
+    private model:string;
+    private trademark:string;
+    private sdSize:number;
+    private color:string;
+    private is5G:boolean;
+    private cameraNumber:number;
+    private price:number;
 
     //CONSTRUCTOR
     constructor(    nombre:string,
-                    modelo:string,
-                    trademark:string = null,
-                    sdSize:number = 8,
-                    color:string = "black",
-                    is5G:boolean = false,
-                    cameraNumber:number = 2,
-                    price:number = 200
-               ){
-
-    }
-
-    //METODO 
-    public print():void
+                    modelo:string, )
     {
-        let atributos = Object.getOwnPropertyNames(this);
-
-        for(let atr in atributos)
-        {  
-            console.log(atributos[atr] + ": " + this[atributos[atr]]);
-        }
+        this.name = nombre;
+        this.model = modelo;
+        this.trademark = null;
+        this.sdSize = 8;
+        this.color = "black";
+        this.is5G = false;
+        this.cameraNumber = 2;
+        this.price = 200;
     }
 
+    //GETTERS & SETTERS
+    //Get----------------
+
+    public getName():string
+    {
+        return this.name;
+    }
+
+    public getModel():string
+    {
+        return this.model;
+    }
+
+    public getTrademark():string
+    {
+        return this.trademark;
+    }
+
+    public getSdSize():number
+    {
+        return this.sdSize;
+    }
+
+    public getColor():string
+    {
+        return this.color;
+    }
+
+    public getIs5G():boolean
+    {
+        return this.is5G;
+    }
+
+    public getCameraNum():number
+    {
+        return this.cameraNumber;
+    }
+
+    public getPrice():number
+    {
+        return this.price;
+    }
+
+    //Set--------------------------
+    public setName(nombre:string):void
+    {
+        this.name = nombre;
+    }
+
+    public setModel(modelo:string):void
+    {
+        this.model = modelo;
+    }
+
+    public setTrademark(marca:string):void
+    {
+        this.trademark = marca;
+    }
+
+    public setSdSize(tam:number):void
+    {
+        this.sdSize = tam;
+    }
+
+    public setColor(color:string):void
+    {
+        this.color = color;
+    }
+
+    public setIs5G(s:boolean):void
+    {
+        this.is5G = s;
+    }
+
+    public setCameraNum(num:number):void
+    {
+        this.cameraNumber = num;
+    }
+
+    public setPrice(precio:number):void
+    {
+        this.price = precio;
+    }
+
+    //METODOS
+
+    public print():string
+    {
+        let imprim: string = "The characteristics of the mobile" 
+                            + this.getName() + " " 
+                            + this.getModel() + " are: \n";
+        
+        let misAtr = Object.getOwnPropertyNames(this);
+        // console.log(misAtr);
+
+        for(let atr in misAtr)
+        {
+            let letra = (misAtr[atr].charAt(0)).toLocaleUpperCase();
+            
+            let resto = misAtr[atr].substr(1);
+            
+            imprim += (letra + resto) + ": " + this[misAtr[atr]] + "\n";
+        }
+
+        return imprim;
+    }
 }
--- testMobile.ts
+++ testMobile.ts
@@ -1,36 +1,47 @@
 import { Mobile } from "./mobile";
-
+// Probando Set
 let movil1 = new Mobile("Nokia", "3210");
-movil1.trademark = "Nokia";
-movil1.sdSize = 12;
-movil1.color ="rojo";
-movil1.is5G = false;
-movil1.cameraNumber = 2;
-movil1.price = 250;
+movil1.setTrademark( "Nokia" );
+movil1.setSdSize(12);
+movil1.setColor("rojo");
+movil1.setIs5G(false);
+movil1.setCameraNum(2);
+movil1.setPrice(250);
 
 let movil2 = new Mobile("Iphone","3G");
-movil2.trademark = "Apple";
-movil2.sdSize = 24;
-movil2.color ="plata";
-movil2.is5G = false;
-movil2.cameraNumber = 4;
-movil2.price = 650;
+movil2.setTrademark("Apple");
+movil2.setSdSize(24);
+movil2.setColor("plata");
+movil2.setIs5G(false);
+movil2.setCameraNum(4);
+movil2.setPrice(650);
 
 let movil3 = new Mobile("Samsung","Galaxy 10");
-movil3.trademark = "Samsung";
-movil3.sdSize = 48;
-movil3.color ="rosa";
-movil3.is5G = true;
-movil3.cameraNumber = 4;
-movil3.price = 720;
+movil3.setTrademark("Samsung");
+movil3.setSdSize(48);
+movil3.setColor("rosa");
+movil3.setIs5G(true);
+movil3.setCameraNum(4);
+movil3.setPrice(720);
 
-console.log("MOVIL 1----------------"); 
-movil1.print();
+//Probar Get
+console.log("\nProbar Gets********************");
 
-console.log("MOVIL 2----------------");
-movil2.print();
+console.log("nombre-> " + movil1.getName());
+console.log("modelo-> " + movil1.getModel());
+console.log("sdSize-> " + movil1.getSdSize());
+console.log("color-> " + movil1.getColor());
+console.log("Num camaras-> " + movil1.getCameraNum());
+console.log("precio-> " + movil1.getPrice());
 
-console.log("MOVIL 3----------------");
-movil3.print();
+console.log("\nProbar Print********************");
+
+let misMoviles:Mobile[] = [movil1,movil2,movil3];
+for(let mov in misMoviles)
+{
+    console.log("\n------------------------");
+    console.log(misMoviles[mov].print());  
+}
+
 
 
